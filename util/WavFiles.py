cfda6b9df4ce9544740499188104e1eb73ccf111
Wesley Kuhron Jones <jazzzman627@yahoo.com>
2025-01-24 22:47:37 +0000

    trying to fix problems in correlation and offset calculations

--- util/WavFiles.py
+++ util/WavFiles.py
@@ -1,17 +1,32 @@
 import numpy as np
+import wavio
 
 
 RATE = 44100
 MAX_AMPLITUDE = 32767
 
 
-def get_array_from_file(fp, zoom):
+def get_array_from_file(fp):
+    # TODO why is running sliding_rms on this array so much slower than on the one from get_array_from_file_reading_binary_directly?
+    arr = wavio.read(fp).data
+    m, one = arr.shape
+    assert one == 1
+    return arr.reshape((m,))
+
+
+def get_array_from_file_reading_binary_directly(fp, zoom_or_audacity="zoom"):
+    if zoom_or_audacity not in ["zoom", "audacity"]:
+        raise ValueError(f"Invalid recorder specified: {zoom_or_audacity = !r}, but needs to be either 'zoom' (for Zoom H5 or H6 recorder) or 'audacity' (for Audacity program)")
+
     print(f"opening {fp}")
     with open(fp, "rb") as f:
         contents = f.read()
 
     hx = contents.hex()
-    padding = 65536 if zoom else 22  # Audacity uses a different value for some reason
+
+    # TODO/FIXME there might be bug here due to hardcoding the number of bytes used for padding by different recording devices
+    # ideally use a library to get wav data
+    padding = 65536 if zoom_or_audacity else 22  # Audacity uses a different value for some reason
 
     samples = len(hx) / 4 - padding
     assert samples % 1 == 0, f"samples should be an integer, got {samples}"
