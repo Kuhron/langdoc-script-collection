aa7fed0b2735478657cd87e4c42becd36a5fc98d
Scearp <cefperry@gmail.com>
2025-04-08 15:09:27 -0700

    clarify prompt for deleting temp files and change behavior of confirmation function

--- util/CliUtil.py
+++ util/CliUtil.py
@@ -1,10 +1,9 @@
 # functions for command line interaction
 
 
-def confirm_action(prompt=None):
-    if prompt is not None:
-        print(prompt)
-    print("press enter to continue, or type something and then press enter to abort")
+def confirm_action(prompt):
+    # empty input defaults to yes, any input other than y or Y defaults to no.
+    print(prompt)
+    print("Type [y/n] and press enter: ")
     inp = input()
-    return inp == ""
-
+    return (inp == "") or (inp.upper() == "Y")
--- util/VideoAudioAligningOrganization.py
+++ util/VideoAudioAligningOrganization.py
@@ -6,7 +6,6 @@ from typing import Tuple
 from util.CliUtil import confirm_action
 
 
-
 def get_tmp_dir_path(text_dir: Path, create_if_absent=False):
     # assume we are dealing with only one video and potentially multiple audios, so we will name the temp dir after the video file
     path = text_dir / ".tmp"
@@ -21,7 +20,7 @@ def create_tmp_dir(tmp_dir_path):
 
 
 def delete_tmp_dir(tmp_dir_path):
-    if confirm_action(f"the temporary files in {tmp_dir_path} will be removed"):
+    if confirm_action(f"Would you like to delete the temporary files in {tmp_dir_path}? This will not delete the aligned .MTS file."):
         shutil.rmtree(tmp_dir_path)  # be careful to put the right path here!
         print("temporary files have been removed")
     else:
@@ -32,20 +31,22 @@ def get_single_audio_and_video_fps_from_text_dir(text_dir: Path, audio_ext: str,
     audio_fps = list(text_dir.glob("*" + audio_ext))
     if len(audio_fps) != 1:
         zero = len(audio_fps) == 0
-        error_str = f"there should be exactly one audio file ({audio_ext}) in the directory, but found " + ("none" if zero else f"the following {len(audio_fps)}:")
+        error_str = f"there should be exactly one audio file ({audio_ext}) in the directory, but found " + (
+            "none" if zero else f"the following {len(audio_fps)}:")
         for fp in audio_fps:
             error_str += str(fp) + "\n"
         raise Exception(error_str)
     video_fps = list(text_dir.glob("*" + video_ext))
     if len(video_fps) != 1:
         zero = len(video_fps) == 0
-        error_str = f"there should be exactly one video file ({video_ext}) in the directory, but found " + ("none" if zero else f"the following {len(video_fps)}:")
+        error_str = f"there should be exactly one video file ({video_ext}) in the directory, but found " + (
+            "none" if zero else f"the following {len(video_fps)}:")
         for fp in video_fps:
             error_str += str(fp) + "\n"
         raise Exception(error_str)
 
-    audio_fp ,= audio_fps
-    video_fp ,= video_fps
+    audio_fp, = audio_fps
+    video_fp, = video_fps
 
     if not audio_fp.is_absolute():
         warn("audio fp is not absolute")
@@ -53,4 +54,3 @@ def get_single_audio_and_video_fps_from_text_dir(text_dir: Path, audio_ext: str,
         warn("video fp is not absolute")
 
     return audio_fp, video_fp
-
